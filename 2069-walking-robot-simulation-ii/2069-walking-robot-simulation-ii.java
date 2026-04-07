9125367ac2a1d652d513a5521edea8295fa5263f
Kartik Borse <kartikborse056@gmail.com>
2026-04-08 00:39:15 +0530

    Time: 73 ms (46.85%), Space: 57.6 MB (83.92%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -0,0 +1,55 @@
+class Robot {
+
+    int width, height;
+    int x, y;
+    int dir; // 0=East,1=North,2=West,3=South
+
+    int[] dx = {1, 0, -1, 0};
+    int[] dy = {0, 1, 0, -1};
+    String[] dirs = {"East", "North", "West", "South"};
+
+    int perimeter;
+
+    public Robot(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.x = 0;
+        this.y = 0;
+        this.dir = 0; // East
+        this.perimeter = 2 * (width + height) - 4;
+    }
+
+    public void step(int num) {
+        num %= perimeter;
+
+        // special case
+        if (num == 0) {
+            if (x == 0 && y == 0) {
+                dir = 3; // South
+            }
+            return;
+        }
+
+        while (num > 0) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            // check boundary
+            if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
+                dir = (dir + 1) % 4; // turn left
+            } else {
+                x = nx;
+                y = ny;
+                num--;
+            }
+        }
+    }
+
+    public int[] getPos() {
+        return new int[]{x, y};
+    }
+
+    public String getDir() {
+        return dirs[dir];
+    }
+}
