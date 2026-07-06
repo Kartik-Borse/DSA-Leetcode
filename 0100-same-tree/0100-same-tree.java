89feed4252b736e3a1a79879c38541bb7850e903
Kartik Borse <kartikborse056@gmail.com>
2026-07-06 16:19:00 +0530

    Time: 0 ms (100%), Space: 42.5 MB (93.61%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -21,9 +21,29 @@ class Solution {
         if(p==null || q==null){
             return false;
         }
-        if(p.val!=q.val){
+        Queue<TreeNode>q1=new LinkedList<>();
+        Queue<TreeNode>q2=new LinkedList<>();
+        q1.add(p);
+        q2.add(q);
+       while(!q1.isEmpty() && !q2.isEmpty()){
+        TreeNode temp1=q1.poll();
+        TreeNode temp2=q2.poll();
+        if(temp1.val!=temp2.val){
             return false;
         }
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        if(temp1.left!=null && temp2.left!=null){
+            q1.add(temp1.left);
+            q2.add(temp2.left);
+        }else if(temp1.left!=null || temp2.left!=null){
+            return false;
+        }
+        if(temp1.right!=null && temp2.right!=null){
+            q1.add(temp1.right);
+            q2.add(temp2.right);
+        }else if(temp1.right!=null || temp2.right!=null){
+            return false;
+        }
+       }
+       return true;
     }
 }
